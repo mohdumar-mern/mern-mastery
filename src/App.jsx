f3493822740b8587000b2afeb1022a70986caa65
Mohd88umar <214827861+mohdumar-mern-dev@users.noreply.github.com>
2025-07-30 12:50:19 +0530

    Routing and ToastContainer setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-   <h1 className='text-center text-4xl font-black'>Hello coders....</h1>
-    </>
+    <div className="min-h-screen bg-gray-100">
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/about' element={<h1>About</h1>} />
+        <Route path='/contact' element={<h1> Contact</h1>} />
+      </Routes>
+      <ToastContainer />
+    </div>
   )
 }
 
